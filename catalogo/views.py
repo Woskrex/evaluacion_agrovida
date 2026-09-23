19ccd1cf2dafa5adb03d95f759225843e16e29a1
brian burgos <brian.burgos04@inacapmail.cl>
2026-09-23 18:06:02 -0300

    Commit 3: git add . y luego git commit -m "Crear vistas y templates HTML" mas o menos

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def vista_detalle(request):
+    return render(request, 'detalle.html')
+
+def vista_productos(request):
+    return render(request, 'productos.html')
+
+def vista_contacto(request):
+    return render(request, 'contacto.html')
